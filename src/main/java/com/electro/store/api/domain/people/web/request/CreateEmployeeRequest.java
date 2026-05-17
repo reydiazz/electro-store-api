1f41ab18228b7abb5cab1b855a4a2c95ee15601e
reydiazz <angheloreynoso11@gmail.com>
2026-05-17 18:49:43 -0500

    feat: crear peticiones y respuestas del dominio people

---
+++ src/main/java/com/electro/store/api/domain/people/web/request/CreateEmployeeRequest.java
@@ -0,0 +1,19 @@
+package com.electro.store.api.domain.people.web.request;
+
+import com.electro.store.api.domain.people.model.enums.EmployeePosition;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record CreateEmployeeRequest(
+        @NotBlank(message = "Employee data is required")
+        CreatePersonRequest person,
+        @NotNull(message = "Employee position is required")
+        EmployeePosition position,
+        @NotNull(message = "Employee salary not be empty")
+        @DecimalMin(value = "0.01", message = "Employee salary  must be greater than zero")
+        BigDecimal salary
+) {
+}
---
+++ src/main/java/com/electro/store/api/domain/people/web/request/CreatePersonRequest.java
@@ -0,0 +1,14 @@
+package com.electro.store.api.domain.people.web.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreatePersonRequest(
+        @NotBlank(message = "Person first name is required")
+        String firstName,
+        @NotBlank(message = "Person last name is required")
+        String lastName,
+        @NotBlank(message = "Person national id is required")
+        String nationalId,
+        String phone
+) {
+}
