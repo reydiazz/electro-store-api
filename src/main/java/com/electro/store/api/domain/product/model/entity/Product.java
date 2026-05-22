442f8e47c62cff6db90e8a4a326ea4b932571343
Chaufa_FM <fabricio.moron.3@gmail.com>
2026-05-21 21:54:55 -0500

    crear entidades del dominio product

---
+++ src/main/java/com/electro/store/api/domain/product/model/entity/Product.java
@@ -0,0 +1,73 @@
+package com.electro.store.api.domain.product.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+    @Id
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_code", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    @Column(name = "sale_price", nullable = false)
+    private BigDecimal salePrice;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "warranty_months", nullable = false)
+    private Integer warrantyMonths;
+
+    public Product (String code, ProductCategory category, String name, String brand,
+                    String model, BigDecimal salePrice, Integer stock, String description,
+                    Integer warrantyMonths)
+    {
+     this.code = code;
+     this.category = category;
+     this.name = name;
+     this.brand = brand;
+     this.model = model;
+     this.salePrice = salePrice;
+     this.stock = stock;
+     this.description = description;
+     this.warrantyMonths = warrantyMonths;
+    }
+
+    public void update (ProductCategory category, String name, String brand,
+                        String model, BigDecimal salePrice, Integer stock, String description,
+                        Integer warrantyMonths)
+    {
+        this.category = category;
+        this.name = name;
+        this.brand = brand;
+        this.model = model;
+        this.salePrice = salePrice;
+        this.stock = stock;
+        this.description = description;
+        this.warrantyMonths = warrantyMonths;
+    }
+
+}
