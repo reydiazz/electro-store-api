5ac18cd061a77db45c824f170543aa70b0293ab7
Chaufa_FM <fabricio.moron.3@gmail.com>
2026-05-22 09:05:40 -0500

    crear repositorios del dominio product

---
+++ src/main/java/com/electro/store/api/domain/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.electro.store.api.domain.product.repository;
+
+import com.electro.store.api.domain.product.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
