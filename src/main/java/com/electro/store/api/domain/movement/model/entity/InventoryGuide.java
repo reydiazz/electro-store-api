e61fa84e40e58b16462fc0486832846685d46d38
Chaufa_FM <fabricio.moron.3@gmail.com>
2026-05-31 00:36:31 -0500

    feat: crear entidades del dominio movement

---
+++ src/main/java/com/electro/store/api/domain/movement/model/entity/InventoryGuide.java
@@ -0,0 +1,56 @@
+package com.electro.store.api.domain.movement.model.entity;
+
+import com.electro.store.api.domain.auth.model.entity.User;
+import com.electro.store.api.domain.movement.model.enums.GuideType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "inventory_guides")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class InventoryGuide {
+
+    @Id
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_code", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private GuideType type;
+
+    @Column(name = "reason")
+    private String reason;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "guide_date", nullable = false)
+    private LocalDateTime guideDate;
+
+    public InventoryGuide(
+            String code,
+            User user,
+            GuideType type,
+            String reason,
+            String description,
+            LocalDateTime guideDate
+    ) {
+        this.code = code;
+        this.user = user;
+        this.type = type;
+        this.reason = reason;
+        this.description = description;
+        this.guideDate = guideDate;
+    }
+
+
+}
---
+++ src/main/java/com/electro/store/api/domain/movement/model/enums/GuideType.java
@@ -0,0 +1,6 @@
+package com.electro.store.api.domain.movement.model.enums;
+
+public enum GuideType {
+    ENTRY,
+    EXIT
+}
