0dcbc16fab326f4f212ca6cc8b18d94313fbb189
Fabrizio7S <leon.mallma14@gmail.com>
2026-06-01 12:07:04 -0500

    feat: correccion en crear entidades de suppliers

--- src/main/java/com/electro/store/api/domain/buys/model/entity/Suppliers.java
+++ src/main/java/com/electro/store/api/domain/buys/model/entity/Suppliers.java
@@ -1,4 +1,48 @@
 package com.electro.store.api.domain.buys.model.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "suppliers")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Suppliers {
+
+    @Id
+    @Column(name = "code", unique = true, nullable = false)
+    private String code;
+
+    @Column(name = "tax_id")
+    private String taxId;
+
+    @Column(name = "trade_name", nullable = false)
+    private String tradeName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "legal_name")
+    private String legalName;
+
+    public Suppliers(String code, String taxId, String tradeName, String phone, String legalName) {
+        this.code = code;
+        this.taxId = taxId;
+        this.tradeName = tradeName;
+        this.phone = phone;
+        this.legalName = legalName;
+    }
+
+    public void update(String taxId, String tradeName, String phone, String legalName) {
+        this.taxId = taxId;
+        this.tradeName = tradeName;
+        this.phone = phone;
+        this.legalName = legalName;
+    }
 }
