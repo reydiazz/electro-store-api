0ef8a954ba776a2c73f8876fa214bf60acba99ee
reydiazz <angheloreynoso11@gmail.com>
2026-06-01 21:09:02 -0500

    refactor: peticion y respuesta de clientes actualizada

--- src/main/java/com/electro/store/api/domain/people/web/request/UpdateCustomerRequest.java
+++ src/main/java/com/electro/store/api/domain/people/web/request/UpdateCustomerRequest.java
@@ -1,12 +1,10 @@
 package com.electro.store.api.domain.people.web.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateCustomerRequest(
         @NotNull(message = "Customer data is required")
         UpdatePersonRequest person,
-        @NotBlank(message = "Customer tax id is required")
         String taxId
 ) {
 }
