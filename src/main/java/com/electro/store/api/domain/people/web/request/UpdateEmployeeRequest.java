b3d0862160ae5f019b048882c2f7b9f6568edeca
reydiazz <angheloreynoso11@gmail.com>
2026-06-01 20:54:17 -0500

    refactor: peticion de creacion de empleados actualizada

--- src/main/java/com/electro/store/api/domain/people/web/request/UpdateEmployeeRequest.java
+++ src/main/java/com/electro/store/api/domain/people/web/request/UpdateEmployeeRequest.java
@@ -2,13 +2,12 @@ package com.electro.store.api.domain.people.web.request;
 
 import com.electro.store.api.domain.people.model.enums.EmployeePosition;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record UpdateEmployeeRequest(
-        @NotBlank(message = "Employee data is required")
+        @NotNull(message = "Employee data is required")
         UpdatePersonRequest person,
         @NotNull(message = "Employee position is required")
         EmployeePosition position,
