59f45f7f39227caa2f9fd0b2d43c13025a343465
reydiazz <angheloreynoso11@gmail.com>
2026-06-02 09:56:23 -0500

    refactor: nuevas reglas en las columnas de las tablas

--- src/test/java/com/electro/store/api/ApplicationTests.java
+++ src/test/java/com/electro/store/api/ApplicationTests.java
@@ -1,48 +1,41 @@
 package com.electro.store.api;
 
-import com.electro.store.api.domain.auth.model.entity.User;
-import com.electro.store.api.domain.auth.model.enums.Role;
-import com.electro.store.api.domain.auth.repository.UserRepository;
-import com.electro.store.api.domain.buys.repository.SupplierRepository;
-import com.electro.store.api.domain.buys.service.SupplierService;
-import com.electro.store.api.domain.buys.web.request.CreateSupplierRequest;
-import com.electro.store.api.domain.buys.web.request.UpdateSupplierRequest;
-import com.electro.store.api.domain.buys.web.response.SupplierResponse;
+import com.electro.store.api.domain.auth.service.UserService;
+import com.electro.store.api.domain.auth.web.request.CreateUserRequest;
 import com.electro.store.api.domain.people.model.entity.Employee;
 import com.electro.store.api.domain.people.model.enums.EmployeePosition;
 import com.electro.store.api.domain.people.service.EmployeeService;
 import com.electro.store.api.domain.people.web.request.CreateEmployeeRequest;
 import com.electro.store.api.domain.people.web.request.CreatePersonRequest;
-import com.electro.store.api.domain.people.web.response.EmployeeResponse;
-import com.electro.store.api.shared.utils.CodeGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.math.BigDecimal;
-
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Autowired
-	private SupplierRepository supplierRepository;
+	private EmployeeService employeeService;
 
 	@Autowired
-	private SupplierService supplierService;
+	private UserService userService;
 
 	@Test
-	void createSupplier() {
-
-		UpdateSupplierRequest request = new UpdateSupplierRequest(
-				"20123456789",
-				"Tv Perú",
-				"999888777",
-				"Electro Perú SAC"
-		);
-
-		SupplierResponse response = supplierService.update("SUP2606011501116J863",request);
+	void createUser(){
+		String firstName = "John";
+		String lastName = "Doe";
+		String dni = "74224732";
+		String phone = "+51 987 681 912";
+		String sueldo = "3500";
+		String username = "root";
+		String password = "root";
+		CreatePersonRequest requestPerson = new CreatePersonRequest(firstName, lastName, dni, phone);
+		CreateEmployeeRequest request = new CreateEmployeeRequest(requestPerson, EmployeePosition.MANAGER, new BigDecimal(sueldo));
+		Employee employee = employeeService.create(request);
+		CreateUserRequest requestUser = new CreateUserRequest(username, password, employee.getCode());
+		userService.create(requestUser);
 	}
 
 }
