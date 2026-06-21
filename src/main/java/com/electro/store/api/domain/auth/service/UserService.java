14aee45d0ebc134aa29db224d0e77ff8bc47687d
Fran0808 <fortegaquipse@gmail.com>
2026-06-21 14:37:57 -0500

    fix: pequeños errores de logica

--- src/main/java/com/electro/store/api/domain/auth/service/UserService.java
+++ src/main/java/com/electro/store/api/domain/auth/service/UserService.java
@@ -13,7 +13,7 @@ import com.electro.store.api.shared.utils.CodeGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +26,7 @@ public class UserService {
 
     private final AuthService authService;
     private final EmployeeService employeeService;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public Page<User> findAll(Pageable pageable) {
@@ -37,7 +38,7 @@ public class UserService {
         verifyUsername(request.username());
         Employee employee = employeeService.findByCodeOrThrow(request.employeeCode());
         String code = CodeGenerator.next(PREFIX);
-        String password = new BCryptPasswordEncoder().encode(request.password());
+        String password = passwordEncoder.encode(request.password());
         User user = new User(code, request.username(), password, request.role(), employee);
         return repository.save(user);
     }
@@ -46,7 +47,8 @@ public class UserService {
     public User update(UpdateUserRequest request, String code) {
         verifyUsername(request.username(), code);
         User user = findByCodeOrThrow(code);
-        user.update(request.username(), request.password(), request.role());
+        String password = passwordEncoder.encode(request.password());
+        user.update(request.username(), password, request.role());
         return user;
     }
 
--- src/main/java/com/electro/store/api/domain/auth/web/controller/UserController.java
+++ src/main/java/com/electro/store/api/domain/auth/web/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 @PreAuthorize("hasAnyRole('ADMIN')")
 @RequiredArgsConstructor
