7992736c2fb1f77f6937810babd8d82ed8097cc2
Chaufa_FM <fabricio.moron.3@gmail.com>
2026-07-09 22:20:49 -0500

    feat: agregar consultas de calculo inicial para Sales, Purchase y Guide

--- src/main/java/com/electro/store/api/domain/sales/repository/SaleDetailRepository.java
+++ src/main/java/com/electro/store/api/domain/sales/repository/SaleDetailRepository.java
@@ -2,6 +2,21 @@ package com.electro.store.api.domain.sales.repository;
 
 import com.electro.store.api.domain.sales.model.entity.SaleDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface SaleDetailRepository extends JpaRepository<SaleDetail, String> {
+    @Query("""
+        SELECT COALESCE(SUM(d.quantity), 0)
+        FROM SaleDetail d
+        WHERE d.product.code = :productCode
+          AND d.sale.saleDate < :startDate
+        """)
+    Integer getSoldQuantityBefore(
+            @Param("productCode") String productCode,
+            @Param("startDate") LocalDateTime startDate
+    );
+
 }
